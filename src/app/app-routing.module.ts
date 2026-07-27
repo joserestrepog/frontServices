b133c2da649ffc5ab5495a4834bb1bb066138676
Jose Luis Restrepo Gesamá <lgesama@inforcol.local>
2026-07-27 09:35:48 -0500

    feat: se creó conectó la ruta al módulo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', redirectTo: 'characters', pathMatch: 'full' },
+{path: 'characters', loadChildren: () => import('./characters/characters.module').then(m => m.CharactersModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
